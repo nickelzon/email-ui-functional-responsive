f64f3a07c6e557d626a590fa148054f129a9d1f0
alnickelzon <al.villanueva1234@gmail.com>
2022-11-10 22:35:14 +0800

    changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,14 +3,14 @@ const Header = () => {
     <div className=" border-b-[1px] p-2">
       <div className="flex justify-between items-center mt-2">
         <div className="font-poppins flex button-container text-xs">
-          <input type="checkbox" name="select-all" id="" className="" />
-          <button className="save bg-green-200 border-green-600 text-green-600 border-[1px] px-2  rounded-md">
+          <input type="checkbox" name="select-all" id="" className="child" />
+          <button className="child save bg-green-200 border-green-600 text-green-600 border-[1px] px-2  rounded-md">
             SAVE
           </button>
-          <button className="manage-filter bg-slate-200 border-slate-600 text-slate-600 border-[1px] px-2  rounded-md">
+          <button className="child manage-filter bg-slate-200 border-slate-600 text-slate-600 border-[1px] px-2  rounded-md">
             MANAGE FILTER
           </button>
-          <button className="delete bg-red-200 border-red-600 text-red-600 border-[1px] px-2 rounded-md">
+          <button className="child delete bg-red-200 border-red-600 text-red-600 border-[1px] px-2 rounded-md">
             DELETE
           </button>
         </div>
--- src/emails/email.js
+++ src/emails/email.js
@@ -10,11 +10,11 @@ const email = [
       email: "sbtest.isabel@gmail.com",
     },
     subject: "New Project 3",
-    date: Date("2021-01-05"),
+    date: new Date("2022-11-11"),
     content:
       "대통령의 임기가 만료되는 때에는 임기만료 70일 내지 40일전에 후임자를 선거한다. 직전대통령이 없을 때에는 대통령이 지명한다. 국가안전보장회의는 대통령이 주재한다, 국회는 회계연도 개시 30일전까지 이를 의결하여야 한다.",
     expanded: false,
-    read: true,
+    read: false,
     tags: [],
   },
   {
@@ -28,7 +28,7 @@ const email = [
       email: "sbtest.charles@gmail.com",
     },
     subject: "Concert Ticket",
-    date: Date("2022-07-22"),
+    date: new Date("2021-12-17"),
     content:
       "누구든지 성별·종교 또는 사회적 신분에 의하여 정치적·경제적·사회적·문화적 생활의 모든 영역에 있어서 차별을 받지 아니한다. 사법권은 법관으로 구성된 법원에 속한다, 1차에 한하여 중임할 수 있다. 국무회의는 정부의 권한에 속하는 중요한 정책을 심의한다.",
     expanded: false,
@@ -46,7 +46,7 @@ const email = [
       email: "alvillanueva12345@gmail.com",
     },
     subject: "Let's 1v1",
-    date: Date("2022-11-10"),
+    date: new Date("2022-01-05"),
     content:
       "국회는 국무총리 또는 국무위원의 해임을 대통령에게 건의할 수 있다. 국채를 모집하거나 예산외에 국가의 부담이 될 계약을 체결하려 할 때에는 정부는 미리 국회의 의결을 얻어야 한다. 대통령이 궐위되거나 사고로 인하여 직무를 수행할 수 없을 때에는 국무총리. 모든 국민은 행위시의 법률에 의하여 범죄를 구성하지 아니하는 행위로 소추되지 아니하며.",
     expanded: false,
