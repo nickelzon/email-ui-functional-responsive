76cc805a046137da636528ed22e62ebd87922b96
alnickelzon <al.villanueva1234@gmail.com>
2022-11-10 20:19:48 +0800

    started header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,23 @@
+const Header = () => {
+  return (
+    <div className=" border-b-[1px] p-2">
+      <div className="flex justify-between items-center mt-2">
+        <div className="font-poppins flex button-container text-xs">
+          <input type="checkbox" name="select-all" id="" className="" />
+          <button className="save bg-green-200 border-green-600 text-green-600 border-[1px] px-2  rounded-md">
+            SAVE
+          </button>
+          <button className="manage-filter bg-slate-200 border-slate-600 text-slate-600 border-[1px] px-2  rounded-md">
+            MANAGE FILTER
+          </button>
+          <button className="delete bg-red-200 border-red-600 text-red-600 border-[1px] px-2 rounded-md">
+            DELETE
+          </button>
+        </div>
+        <h1 className="font-poppins"> 50-150 </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
