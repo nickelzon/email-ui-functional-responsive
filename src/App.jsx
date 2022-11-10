b0658d454ba299b8c8f9a502b14fa2cb36e775b8
alnickelzon <al.villanueva1234@gmail.com>
2022-11-10 22:31:45 +0800

    i don't know what i did here

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Components/Header";
+import Body from "./Components/Body";
+
+const App = () => {
+  return (
+    <div className="w-full">
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Body />
+      </div>
+    </div>
+  );
+};
+
+export default App;
