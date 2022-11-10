1f283a313c913cfc4ac826d889fd1380822205dd
alnickelzon <al.villanueva1234@gmail.com>
2022-11-11 00:17:18 +0800

    expandContent function that inverse the boolean value of expanded

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -4,32 +4,25 @@ import Subject from "./Subject";
 import Datebox from "./Datebox";
 
 const Body = () => {
-  const [unread, setUnread] = useState([]);
-  const [read, setRead] = useState([]);
+  const [emails, setEmails] = useState([]);
 
-  //date-formatting
-  // const option = {
-  //   weekday: "long",
-  //   year: "numeric",
-  //   month: "long",
-  //   day: "numeric",
-  // };
-
-  const fetchUnreads = () => {
-    const unreads = email.filter((item) => !item.read);
-
-    setUnread(unreads);
+  const fetchEmails = () => {
+    setEmails(email);
   };
-  const fetchReads = () => {
-    const reads = email.filter((item) => item.read);
-    console.log(reads);
-    setRead(reads);
+  const expandContent = (id) => {
+    setEmails(
+      emails.map((item) =>
+        item.id === id ? { ...item, expanded: !item.expanded } : item
+      )
+    );
   };
 
   useEffect(() => {
-    fetchUnreads();
-    fetchReads();
+    fetchEmails();
   }, []);
+
+  const unread = emails.filter((item) => !item.read);
+
   return (
     <div className="font-poppins border-red-600">
       <h1 className="font-semibold text-slate-400 text-sm">Unread</h1>
@@ -40,26 +33,25 @@ const Body = () => {
             className={`parent border-[1px] p-5 flex items-center rounded ${
               index === unread.length - 1 ? "mb-0" : "mb-2"
             }`}
+            onClick={() => expandContent(item.id)}
           >
             <input type="checkbox" name="select-one" id="" className="child" />
-            <div className="circle child rounded-full bg-green-600" />
-            <Datebox dates={item} />
-            <Subject emails={item} />
+            <div
+              className={`circle child rounded-full ${
+                item.expanded ? "bg-green-600" : "bg-red-600"
+              }`}
+            />
+            <div className="child">
+              <Datebox dates={item} />
+            </div>
+            <div className="child">
+              <Subject emails={item} />
+            </div>
           </div>
         ))}
       </div>
       <h1 className="font-semibold text-slate-400 text-sm">Recently Saved</h1>
-      <div className="">
-        {read.map((item) => (
-          <div
-            key={item.id}
-            className="border-[1px] p-5 flex items-center rounded"
-          >
-            <div className="circle bg-red-600 rounded-full" />
-            {item.sender.name}
-          </div>
-        ))}
-      </div>
+      <div className=""></div>
     </div>
   );
 };
